178b0d0fefc6e64c766a2c182dbb8572f5b2f688
GermanArt08 <germanzl666@gmail.com>
2026-06-30 17:49:54 -0300

    se
    de

--- Practica 33/js/script.js
+++ Practica 33/js/script.js
@@ -1,12 +1,18 @@
-let corrredores = []
+const cantidad = 5;
+let corrredores = [];
 
 let formIngresarDatos = document.querySelector ("#ingresarDatos");
 let boton = document.querySelector ("#boton");
 let lista = document.querySelector ("#lista");
 
+let contador = 0;
+let ganador;
+let promedio;
+let masRapidoProm;
+
 boton .addEventListener ("click", function(e){
     e.preventDefault
-    formIngresarDatos ()
+    ingresarDatos ()
 });
 
 function ingresarDatos() {
@@ -17,7 +23,7 @@ function ingresarDatos() {
         return false;
     }
         
-    let apellido = document.querySelector("apellido").value
+    let apellido = document.querySelector("#apellido").value
     if (apellido == "") {
         alert("El apellido no puede estar vacío");
         return false;
@@ -28,8 +34,47 @@ function ingresarDatos() {
         alert("Ingrese una edad válida");
         return false;
     }
-    let num = document.querySelector("#numero")
+    let num = document.querySelector("#numero").value
     if (num == "" || isNaN(num)) {
         alert("Ingrese un número válido");
+        return false;
     }
+    let tiempo = document.querySelector ("#tiempo").value
+    if (tiempo =="" || isNaN(tiempo)) {
+        alert("Ingrese un tiempo válido");
+        return false;
+    }
+
+    let corredor = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: edad,
+        num: num,
+        tiempo: tiempo,
+    }
+    corrredores.push(corredor)
+    alert ("Datos ingresados correctamente")
+
+    vaciarFormulario();
+}
+function vaciarFormulario () {
+    document.querySelector ("#nombre").value = "";
+    document.querySelector ("#apellido").value = "";
+    document.querySelector ("#edad").value = "";
+    document.querySelector ("#numero").value = "";
+    document.querySelector ("#tiempo").value = "";
+
+    verificarCantDatos();
+}
+function verificarCantDatos() {
+    if (corrredores.length>=cantidad)
+        boton.disabled = true;
+        for (let i=0; i<=cantidad; i++) {
+        contador += tiempo[i];
+}
+promedio = contador/cantidad;
+}
+if(corrredores.lenght>=cantidad) { 
+let listado = document.querySelector(".listado");
+listado.innerHTML = (`El promedio de tiempor es de ${promedio.value}s`)
 }
